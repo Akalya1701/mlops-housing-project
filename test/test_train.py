4cabdea22552e3093666c99cd2efc40f7d60920a
Akalya <akalya.arunachalm@effem.com>
2025-06-07 21:53:19 +0530

    added test code to test the trained model

--- test/test_train.py
+++ test/test_train.py
@@ -1,5 +1,10 @@
-# You can run tests using pytest or unittest.
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+from train import model, X_test, y_test
 
 def test_model_accuracy_threshold():
-    from train import model, X_test, y_test
-    assert model.score(X_test, y_test) > 0.6
+    score = model.score(X_test, y_test)
+    assert score > 0.6, f"Model accuracy too low: {score}"
+
